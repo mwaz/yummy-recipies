8a6283d648108bc69a9a770570434765ab3f0c55
mwaz <amby.mwaz@gmail.com>
2017-10-16 18:10:32 +0300

    [BUG-FIX] remove import redundancies in index.html header

--- app/views.py
+++ app/views.py
@@ -1,11 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 
 
 if __name__ == "__main__":
