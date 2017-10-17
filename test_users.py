defaf94700236632054451f4e3fe816984c3c884
mwaz <amby.mwaz@gmail.com>
2017-10-17 11:25:09 +0300

    [TEST] test null password in user_register function in class Users

--- test_users.py
+++ test_users.py
@@ -27,6 +27,11 @@ class TestUsers(TestCase):
         res = self.newUser.user_register("waweru@gmail.com", " ", "password", "password")
         self.assertEqual(7, res, "Please provide a username")
 
+    def test_null_password(self):
+        """Test to check if email is null"""
+        res = self.newUser.user_register("waweru@gmail.com", "mwaz", " ", "password")
+        self.assertEqual(7, res, "No password provided")
+
 
 
 
