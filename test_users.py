6f99e1fe54d7ca6fd90b19068080e27d3a412bd7
mwaz <amby.mwaz@gmail.com>
2017-10-18 01:18:23 +0300

    [TEST] add test function to test of confirm password is empty

--- test_users.py
+++ test_users.py
@@ -17,30 +17,36 @@ class TestUsers(TestCase):
         res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password","password")
         self.assertEqual(2, res, "User Account Created Successfully")
 
-    def test_empty_email_field(self):
+    def test_null_email_field(self):
         """Test to check if email is null"""
-        res = self.newUser.user_register(" ", "mwaz", "password", "password")
+        res = self.newUser.user_register("", "mwaz", "password", "password")
         self.assertEqual(7, res, "Please provide an email address")
 
-    def test_empty_username(self):
+    def test_null_username(self):
         """Test to check if username is null"""
-        res = self.newUser.user_register("waweru@gmail.com", " ", "password", "password")
-        self.assertEqual(7, res, "Please provide a username")
+        res = self.newUser.user_register("waweru@gmail.com", "", "password", "password")
+        self.assertEqual(8, res, "Please provide a username")
 
-    def test_empty_password(self):
+    def test_null_password(self):
         """Test to check if email is null"""
-        res = self.newUser.user_register("waweru@gmail.com", "mwaz", " ", "password")
+        res = self.newUser.user_register("waweru@gmail.com", "mwaz", "", "password")
         self.assertEqual(7, res, "No password provided")
 
-    def test_empty_cpassword(self):
-        """ Test to check if cpasword is empty """
-        res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password", " ")
+    def test_null_cpassword(self):
+        """ Test to check if cpasword is null """
+        res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password", "")
         self.assertEqual(7, res, "Confirm Password not provided ")
 
     def test_cpassword_is_equal_to_password(self):
         """Test to check whether the confirm password and the passwords are similar """
         res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password", "pass")
         self.assertEqual(4, res, "The passwords do not match")
+
+    def test_cpassword_is_empty(self):
+        """ Test to check whether confirm password field is empty"""
+        res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password", " ")
+        self.assertEqual(7, res, "Confirm Password field is empty")
+
 
 
 
