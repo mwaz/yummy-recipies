6c914c517fa00209c8554ffa28e3cc3136c0f31d
mwaz <amby.mwaz@gmail.com>
2017-10-17 10:39:13 +0300

    [TEST] test user_register function in class Users

--- test_users.py
+++ test_users.py
@@ -1,3 +1,27 @@
+import unittest
 from unittest import TestCase
-from User import Users
+from user import Users
+
+
+class TestUsers(TestCase):
+    """
+    class to test the class user
+    """
+
+    def setUp(self):
+        """ Method called to prepare the test fixture """
+        self.newUser = Users()
+
+    def test_user_register(self):
+        """ Test to test function user_register """
+        res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password","password")
+        self.assertEqual(2, res, "User Account Created Successfully")
+
+
+
+
+
+
+
+
 
