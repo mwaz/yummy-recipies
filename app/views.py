4eb911f9d9347e2ac85d4b180caa507ee147b6ec
mwaz <amby.mwaz@gmail.com>
2017-10-18 16:12:09 +0300

    [FEATURE] add user register functions and capture data from input forms

--- app/views.py
+++ app/views.py
@@ -1,14 +1,56 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, session
+from flask import Flask
 app = Flask(__name__)
+from user import Users
 
+new_user = Users()
 
 @app.route('/')
 def index():
+    """ Redirects to the index page """
+
     return render_template('index.html')
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('registration.html')
+    """ Handles the ciew for user registration"""
+
+    if request.method == ['POST']:
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        cpassword = request.form['cpassword']
+        res = new_user.user_register(email, username, password, cpassword)
+        print(res)
+        if res == 1:
+            session['user'] = username
+            msg_output = "Successfully created Account"
+            print (msg_output)
+            return render_template('registration.html', msg = msg_output)
+        elif res == 7:
+            msg_output = "please fill in all the fields"
+            return render_template("registration.html", msg = msg_output)
+        elif res == 8:
+            msg_output = "Special characters are not allowed in username"
+            return render_template("registration.html", msg = msg_output)
+        elif res == 2:
+            msg_output = "Password should have atleat 8 characters with at least a letter and a number"
+            return render_template("registration.html", msg = msg_output)
+        elif res == 6:
+            msg_output = "Username already taken"
+            print("ok")
+            return render_template("registration.html", msg = msg_output)
+        elif res == 4:
+            msg_output = "Passwords dont match"
+            return render_template("registration.html", msg = msg_output)
+        elif res == 3:
+            msg_output = "Email is not a valid email in the format someone@someone.com"
+            return render_template("registration.html", msg = msg_output)
+        elif res == 5:
+            msg_output = "Email already exists"
+            return render_template("registration.html", msg = msg_output)
+    return render_template("registration.html")
 
 @app.route('/login')
 def login():
@@ -22,8 +64,5 @@ def recipeCategories():
 def recipes():
     return render_template('recipes.html')
 
-
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
