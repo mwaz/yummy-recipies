c64f31cf4b0151ea4e61d91ab6126a4fda992af4
mwaz <amby.mwaz@gmail.com>
2017-10-19 18:00:36 +0300

    [TEST] add method setup to initialize the test file

--- test_recipes.py
+++ test_recipes.py
@@ -8,4 +8,9 @@ class RecipeTest(unittest.TestCase):
        Class performing unit testing for class Recipe
     """
 
+    def setUp(self):
+        """Defining setUp() method that runs prior to each test."""
+        self.recipes = Recipe()
+        self.recipes.Recipes = {}
+
    
