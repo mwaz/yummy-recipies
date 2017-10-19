ec6708554b0bc43250d2c35583715ed0c1e271ad
mwaz <amby.mwaz@gmail.com>
2017-10-19 16:11:19 +0300

    [FEATURE] add a method to create a recipe category that will hold the recipes

--- recipes.py
+++ recipes.py
@@ -15,4 +15,36 @@ class Recipe(object):
         self.owner = recipes_owner
         self.recipe = recipe
 
-   
+    def create (self, recipe_category, recipes_owner):
+        """defining method to create  a recipe category"""
+        if re.match("[a-zA-Z0-9- .]+$", recipe_category):
+            if recipe_category != '':
+                # call the get_recipes function that contains individual recipes
+                recipe_categories = self.get_recipes(recipes_owner)
+                if recipe_categories != {}: #returns all recipe categories
+                    #check if a user has a recipes in a recipe category
+                    if recipe_category not in recipe_categories.keys():
+                        self.Recipes[recipe_category] = {"recipe_category": recipe_category, "recipes_owner": recipes_owner,}
+                        return 1
+                    return 2
+                else:
+                    #Users first recipe category
+                    self.Recipes[recipe_category] = {"recipe_category": recipe_category, "recipes_owner": recipes_owner}
+                    return 1
+            return 3
+        return 4
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
