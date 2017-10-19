d8f15a07b3593a81e50d60a914136ef5bcfd57c0
mwaz <amby.mwaz@gmail.com>
2017-10-19 16:09:52 +0300

    [FEATURE] add constructor method to initialize the class variables

--- recipes.py
+++ recipes.py
@@ -2,3 +2,17 @@ import re
 
 Recipeitems = []
 # an empty lists which stores recipes
+
+
+class Recipe(object):
+    Recipes = {}
+    shared_recipes = {}
+
+    def __init__(self, recipe_category = None, recipes_owner = None, recipe = None):
+        """Initialization of recipe class variables"""
+
+        self.recipe_category = recipe_category
+        self.owner = recipes_owner
+        self.recipe = recipe
+
+   
