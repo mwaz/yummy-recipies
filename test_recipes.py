0f6ef6542ebe4ccb359ff94b96bc9b6e898cc482
mwaz <amby.mwaz@gmail.com>
2017-10-19 17:59:34 +0300

    [FEATURE] add test file to test class Recipe

---
+++ test_recipes.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest import TestCase
+from recipes import Recipe
+
+
+class RecipeTest(unittest.TestCase):
+    """
+       Class performing unit testing for class Recipe
+    """
+
+   
