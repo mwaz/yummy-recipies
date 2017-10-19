d840c2a9999582993362d5e0be4965c09e28f5a7
mwaz <amby.mwaz@gmail.com>
2017-10-19 18:01:17 +0300

    [TEST] add method to test creating a recipe category

--- test_recipes.py
+++ test_recipes.py
@@ -13,4 +13,9 @@ class RecipeTest(unittest.TestCase):
         self.recipes = Recipe()
         self.recipes.Recipes = {}
 
-   
+    def test_for_creating_a_recipe_category(self):
+        """  method definition to test for Creating a recipe category """
+        output = self.recipes.create('recipe_category', 'waweru@gmail.com')
+        self.assertEqual(4, output, "Recipe category successfully created")
+
+ 
