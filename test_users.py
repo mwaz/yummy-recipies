e4fc8e83ad728fea4c3a0349074174d4ae62f3d1
mwaz <amby.mwaz@gmail.com>
2017-10-20 10:48:17 +0300

    [TEST] to check the wrong user password method in users class

--- test_users.py
+++ test_users.py
@@ -40,9 +40,23 @@ class TestUsers(unittest.TestCase):
     def test_cpassword_is_equal_to_password(self):
         """Test to check whether the confirm password and the passwords are similar """
         res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password", "pass")
-        self.assertEqual(4, res, "The passwords do not match")
+        self.assertEqual(4, res, "Invalid credentials, kindly try again")
 
     def test_cpassword_is_empty(self):
         """ Test to check whether confirm password field is empty"""
         res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password", " ")
         self.assertEqual(7, res, "Confirm Password field is empty")
+
+    def test_pass_in_login(self):
+        """method to test whether registration password is equal to login password"""
+        self.newUser.users = {}
+        self.newUser.user_register('waweru@gmail.com', 'mwaz', 'pass', 'pass')
+        result = self.newUser.user_login('waweru@gmail.com', 'pass123')
+        self.assertEqual(3, result, "Wrong login credentials")
+
+    def test_wrong_login_email(self):
+        """defining method to test if login email is equal to register email"""
+        self.newUser.users = {}
+        self.newUser.register('muthomi@gmail.com', 'muthomi', 'pass','pass')
+        result = self.newUser.login('muthomimate@gmail.com', 'pass')
+        self.assertEqual(3, result, "wrong login credentials")
