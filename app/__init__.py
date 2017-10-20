765ec475ddc939c4b1c8d31b767d2d7d0a49a729
mwaz <amby.mwaz@gmail.com>
2017-10-20 13:01:57 +0300

    [FIX] import views.py from app

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from app import views
+
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
+
+#initialize views
+from app import views
 
 # Load the config file
 app.config.from_object('config')
