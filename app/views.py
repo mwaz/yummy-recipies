0297a8bae8e07bbebab8a91bd5a95703ba9c179e
mwaz <amby.mwaz@gmail.com>
2017-10-20 19:45:56 +0300

    [FEATURE] add method to handle creation of a recipe category

--- app/views.py
+++ app/views.py
@@ -1,11 +1,15 @@
 import os
+import random
 from flask import render_template, redirect, request, session, g, url_for
 from flask import Flask
 app = Flask(__name__)
 from user import Users
+from recipe import Recipe
 from app import app
 
+category_id = random.choice(range(0, 50))
 new_user = Users()
+new_cat = Recipe()
 """Objects Instatiation"""
 app.secret_key = os.urandom(24)
 
@@ -29,7 +33,6 @@ def register():
         if res == 1:
             session['user'] = username
             msg_output = "Successfully created Account"
-
             return render_template('login.html', msg=msg_output)
         elif res == 7:
             msg_output = "please fill in all the fields"
@@ -105,3 +108,34 @@ def logout():
     """ method to logout a user"""
     session.pop('user', None)
     return redirect(url_for('login'))
+
+@app.route('/cat_register', methods=['GET', 'POST'])
+def category_register():
+    """ Method to create a category """
+    if request.method == "POST":
+        cat_name = request.form['category_name']
+        cat_id = request.form['category_name']
+        owner = request.form['category_owner']
+
+        category_create = new_cat .category_register(cat_name,cat_id, owner)
+        if category_create == 1:
+            message = "Successfully created category"
+            return render_template("recipe-categories.html", msg=message)
+
+        elif category_create == 2:
+            message = "Category id already exists"
+            return render_template("recipe-categories.html", msg=message)
+        elif category_create == 3:
+            message = "Category already exists"
+            return render_template("recipe_categories", msg=message)
+        elif category_create == 4:
+            message = "Kindly input category name"
+            return render_template("recipe_categories.html", msg=message)
+        elif category_create == 5:
+            message = "No special characters allowed in category name"
+            return render_template("recipe_categories.html", msg=message)
+        else:
+            message = "unable to create category"
+            return render_template("recipe_categories", msg=message)
+    return render_template("recipe_categories.html")
+
