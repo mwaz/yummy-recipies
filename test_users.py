3479c4e3c63fa45aceaf3e5c3392831593941cba
mwaz <amby.mwaz@gmail.com>
2017-10-20 14:41:10 +0300

    [TEST] add method to test empty email input in login page

--- test_users.py
+++ test_users.py
@@ -8,12 +8,14 @@ class TestUsers(unittest.TestCase):
     class to test the class user
     """
 
+
     def setUp(self):
         """ Method called to prepare the test fixture """
         self.newUser = Users()
 
     def test_user_register(self):
         """ Test to test function user_register """
+        self.newUser.users = {} #users dictionary to check whether the user already exists
         res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password", "password")
         self.assertEqual(2, res, "User Account Created Successfully")
 
@@ -47,16 +49,44 @@ class TestUsers(unittest.TestCase):
         res = self.newUser.user_register("waweru@gmail.com", "mwaz", "password", " ")
         self.assertEqual(7, res, "Confirm Password field is empty")
 
-    def test_pass_in_login(self):
+    def test_success_login(self):
+        """ method to test a successful login """
+        self.newUser.users = {}
+        self.newUser.user_register('waweru@gmail.com', 'mwaz', 'pass', 'pass')
+        result = self.newUser.user_login('waweru@gmail.com', 'pass')
+        self.assertEqual(3, result, "Successful login")
+
+    def test_wrong_pass_login(self):
         """method to test whether registration password is equal to login password"""
         self.newUser.users = {}
         self.newUser.user_register('waweru@gmail.com', 'mwaz', 'pass', 'pass')
         result = self.newUser.user_login('waweru@gmail.com', 'pass123')
         self.assertEqual(3, result, "Wrong login credentials")
 
-    def test_wrong_login_email(self):
-        """defining method to test if login email is equal to register email"""
+    def test_wrong_email_login(self):
+        """ method to test if the email used for login is wrong"""
         self.newUser.users = {}
-        self.newUser.register('muthomi@gmail.com', 'muthomi', 'pass','pass')
-        result = self.newUser.login('muthomimate@gmail.com', 'pass')
+        self.newUser.user_register('waweru@gmail.com', 'mwaz', 'pass','pass')
+        result = self.newUser.user_login('wawerum@gmail.com', 'pass')
         self.assertEqual(3, result, "wrong login credentials")
+
+    def test_null_email_login(self):
+        """ method to test if the email used for login is null"""
+        result = self.newUser.user_login('','pass')
+        self.assertEqual(4,result, "Kindly fill the email field")
+
+    def test_null_password(self):
+        """ method to test if the password used for login is null """
+        result = self.newUser.user_login('waweru@gmail.com', '')
+        self.assertEqual(4, result, "Kindly fill the password field")
+
+    def test_null_inputs(self):
+        """ method to test if both the password and login is null """
+        result = self.newUser.user_login('', '')
+        self.assertEqual(4, result, "Kindly fill in all fields")
+
+    def test_empty_email(self):
+        result = self.newUser.user_login(' ', 'pass')
+        self.assertEqual(5, result, "Email field is empty")
+
+   
