654e6ac1e67587c68b2c0b9cab69ac8b4bda3918
mwaz <amby.mwaz@gmail.com>
2017-10-22 02:45:02 +0300

    [FIX] refactor status codes and arguments on member_register method

--- user.py
+++ user.py
@@ -1,33 +1,30 @@
 import re
 
 users = {}
-
+regex_member = "[a-zA-Z0-9- .]+$"
+regEmail = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+regPassword = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
 
 class Users(object):
     """
       Users Class to handle  user functions like login and signup
       """
-
-    def __init__(self, username=None, email=None, password=None):
+    def __init__(self, member=None, email=None, password=None):
         """ Initializing  class instance variables"""
-        self.username = username
+        self.member = member
         self.email = email
         self.password = password
 
-    def user_register(self, email, username, password, cpassasword):
+    def member_register(self, email, member, password, cpassasword):
         """ method that defines the elements required to create an account """
-        regUsername = "[a-zA-Z0-9- .]+$"
-        regEmail = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-        regPassword = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-
-        if re.match(regUsername, username):
-            if username != '' and email != '' and password != '' and cpassasword != '' and cpassasword != ' ':
-                if username not in users.keys():
+        if re.match(regex_member, member):
+            if member != '' and email != '' and password != '' and cpassasword != '' and cpassasword != ' ':
+                if member not in users.keys():
                     if email not in users.keys():
                         if password == cpassasword:
                             if re.search(regEmail, email):
                                 if re.search(regPassword, password):
-                                    users[email] = {'username': username,
+                                    users[email] = {'member': member,
                                                     'email': email,
                                                     'password': password,
                                                     }
@@ -38,15 +35,15 @@ class Users(object):
                     return 5
                 return 6
             return 7
-        return 8
+        return "205,Regex mismatch"
 
     def user_login(self, email, password):
         """Method to login user"""
         if email != ' ' and password != ' ':
             if email != '' and password != '':
                 if email in users.keys():
-                    res = users[email]
-                    passwd = res['password']
+                    result = users[email]
+                    passwd = result['password']
                     if passwd == password:
                         return 1
                     return 2
@@ -55,14 +52,14 @@ class Users(object):
         return 5
 
 
-    def get_username(self, email):
+    def get_member(self, email):
         if email in users.keys():
-            resUser = users[email]
-            return resUser['username']
+            result_user = users[email]
+            return result_user['member']
         return False
 
     def get_email(self, email):
         if email in users.keys():
-            resEmail = users[email]
-            return resEmail['email']
+            result_email = users[email]
+            return result_email['email']
         return False
