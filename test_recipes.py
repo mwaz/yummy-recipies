b26984f65fb6a6f17ab64c3c1b47afb8c01f2244
mwaz <amby.mwaz@gmail.com>
2017-10-22 02:22:47 +0300

    [TEST] add test to test for succesful category name registration

--- test_recipes.py
+++ test_recipes.py
@@ -1,24 +1,16 @@
 import unittest
-from unittest import TestCase
-from recipes import Recipe
+from recipe import Recipe
 
 
 class RecipeTest(unittest.TestCase):
-    """
-       Class performing unit testing for class Recipe
-    """
-
+    """ Class performing unit testing for class Recipe"""
     def setUp(self):
         """Defining setUp() method that runs prior to each test."""
-        self.recipes = Recipe()
-        self.recipes.Recipes = {}
+        self.newCat= Recipe()
 
-    def test_for_creating_a_recipe_category(self):
-        """  method definition to test for Creating a recipe category """
-        output = self.recipes.create('recipe_category', 'waweru@gmail.com')
-        self.assertEqual(4, output, "Recipe category successfully created")
-
-    def test_if_recipes_name_is_empty(self):
-        """ method definition to test for adding a recipe with an empty title """
-        output = self.recipes.create('', 'recipe_category_owner')
-        self.assertEqual(4, output, "please fill all fields")
+    def test_category_registration(self):
+        """ Test to test function create category """
+        self.newCat.recipe_categories = {}
+        self.newCat.category_register("category_one", "waweru@gmail.com")
+        category_registration = self.newCat.category_register("category", "waweru@gmail.com")
+        self.assertEqual("200,OK", category_registration, " success in creating category")
