f5eef450517ca8f59432b3f171b006f3d1943dcc
mwaz <amby.mwaz@gmail.com>
2017-10-22 02:29:32 +0300

    [TEST] add test to check if regex of username conforms to the given format

--- test_users.py
+++ test_users.py
@@ -26,7 +26,7 @@ class TestUsers(unittest.TestCase):
     def test_null_member(self):
         """Test to check if member name is null"""
         result = self.newUser.member_register("waweru@gmail.com", "", "password", "password")
-        self.assertEqual(8, result, "Please provide a member name")
+        self.assertEqual("205,Regex mismatch", result, "Please provide a member name")
 
     def test_null_password(self):
         """Test to check if email is null"""
@@ -47,6 +47,11 @@ class TestUsers(unittest.TestCase):
         """ Test to check whether confirm password field is empty"""
         result = self.newUser.member_register("waweru@gmail.com", "mwaz", "password", " ")
         self.assertEqual(7, result, "Confirm Password field is empty")
+
+    def test_invalid_member_name (self):
+        """Test to check if member_name follows the one specified by regex"""
+        member_registration= self.newUser.member_register("waweru@gmail.com", "&#*", "password", "password")
+        self.assertEqual("205,Regex mismatch", member_registration, "member name is invalid")
 
     def test_success_login(self):
         """ method to test a successful login """
