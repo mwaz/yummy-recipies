420bfb41d4e5864616bfa3e4adabb24f4136be6d
mwaz <amby.mwaz@gmail.com>
2017-10-22 15:01:03 +0300

    [FIX] refactor return statments to status codes

--- test_recipes.py
+++ test_recipes.py
@@ -6,34 +6,33 @@ class RecipeTest(unittest.TestCase):
     """ Class performing unit testing for class Recipe"""
     def setUp(self):
         """Defining setUp() method that runs prior to each test."""
-        self.newCat= Recipe()
+        self.newCat = Recipe()
 
     def test_category_registration(self):
         """ Test to test function create category """
         self.newCat.recipe_categories = {}
         self.newCat.category_register("category_one", "waweru@gmail.com")
         category_registration = self.newCat.category_register("category", "waweru@gmail.com")
-        self.assertEqual("200,OK", category_registration, " success in creating category")
+        self.assertIn("200,OK", category_registration, "200,OK")
 
     def test_category_exists(self):
         """ Test to check if category_name exists """
         self.newCat.recipe_categories = {}
         self.newCat.category_register("category", "waweru@gmail.com")
         category_registration = self.newCat.category_register("category", "waweru@gmail.com")
-        self.assertEqual("204,Category exists", category_registration, "category name already exists")
+        self.assertIn("204,Category exists", category_registration, "204,Category exists")
 
     def test_empty_category(self):
         """ Test to check empty category name """
         category_empty = self.newCat.category_register("", "waweru@gmail.com")
-        self.assertEqual("205,Regex mismatch", category_empty, "category name is null")
+        self.assertIn("205,Regex mismatch", category_empty, "205,Regex mismatch")
 
     def test_null_category(self):
         """ Test to test creation of a null category name"""
         category_registration = self.newCat.category_register(" ", "waweru@gmail.com")
-        self.assertEqual("205,Invalid Name", category_registration, "category name is empty")
+        self.assertIn("205,Invalid Name", category_registration, "205,Invalid Name")
 
     def test_invalid_category_name(self):
         """Test to check if category_name follows the one specified by regex"""
         category_registration = self.newCat.category_register("&#*", "waweru@gmail.com")
-        self.assertEqual("205,Regex mismatch", category_registration, "category name is invalid")
-
+        self.assertIn("205,Regex mismatch", category_registration, "205,Regex mismatch")
