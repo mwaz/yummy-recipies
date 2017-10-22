225d026d2fb85151b08a08b7577d8d9f8a0f9b8f
mwaz <amby.mwaz@gmail.com>
2017-10-22 15:07:08 +0300

    [FEATURE] method to view individual recipes in recipe categories

--- app/views.py
+++ app/views.py
@@ -1,14 +1,12 @@
 import os
-from flask import render_template,request, session, g,Flask, url_for
 from user import Users
 from recipe import Recipe
+from flask import render_template, request, session, g, Flask, url_for
 app = Flask(__name__)
 from app import app
 
-
 new_user = Users()
 new_cat = Recipe()
-
 
 """Objects Instatiation"""
 app.secret_key = os.urandom(24)
@@ -19,6 +17,7 @@ def index():
     """ Redirects to the index page """
     return render_template('index.html')
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """ Handles the ciew for user registration"""
@@ -27,36 +26,36 @@ def register():
         member = request.form['username']
         password = request.form['password']
         cpassword = request.form['cpassword']
-        res = new_user.member_register(email, member, password, cpassword)
-        if res == 1:
+        result = new_user.member_register(email, member, password, cpassword)
+        if result == "200,OK":
             session['user'] = member
             msg_output = "Successfully created Account"
             return render_template('login.html', success=msg_output)
-        elif res == 7:
+        elif result == "205,Invalid input":
             msg_output = "please fill in all the fields"
             return render_template("registration.html", msg=msg_output)
 
-        elif res == "205,Regex mismatch":
+        elif result == "205,Regex mismatch":
             msg_output = "Special characters are not allowed in member field"
             return render_template("registration.html", msg=msg_output)
 
-        elif res == 2:
+        elif result == "205,Password Regex mismatch":
             msg_output = "Password should have atleat 8 characters with at least a letter and a number"
             return render_template("registration.html", msg=msg_output)
 
-        elif res == 6:
+        elif result == "401,Member exists":
             msg_output = "member name already taken"
-            print("5")
+
             return render_template("registration.html", msg=msg_output)
-        elif res == 4:
+        elif result == "400,Passwords dont match":
             msg_output = "Passwords dont match"
             return render_template("registration.html", msg=msg_output)
 
-        elif res == 3:
+        elif result == "205,Email Regex mismatch":
             msg_output = "Email is not a valid email in the format someone@someone.com"
             return render_template("registration.html", msg=msg_output)
 
-        elif res == 5:
+        elif result == "401,Email exists":
             msg_output = "Email already exists"
             return render_template("registration.html", msg=msg_output)
 
@@ -69,8 +68,8 @@ def login():
     if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
-        resLogin = new_user.user_login(email, password)
-        if resLogin == 1:
+        result_login = new_user.user_login(email, password)
+        if result_login == "200,OK":
             member = new_user.get_member(email)
             email = new_user.get_email(email)
             session['user'] = member
@@ -78,13 +77,13 @@ def login():
             message = "login successful"
             return render_template("recipe-categories.html", success=message)
 
-        elif resLogin == 2:
-            message = "Passwords do not match"
+        elif result_login == "205,Password match":
+            message = "Invalid login credentials"
             return render_template("login.html", msg=message)
-        elif resLogin == 3:
+        elif result_login == "404,User not found":
             message = "User does not exist, kindly try again"
             return render_template("login.html", msg=message)
-        elif resLogin == 4:
+        elif result_login == "205,Invalid input":
             message = "Kindly fill all fields"
             return render_template("login.html", msg=message)
         else:
@@ -96,9 +95,9 @@ def login():
 @app.before_request
 def before_request():
     """Method to declare sessions"""
-    g.user = None
+    g.member = None
     if 'user' in session:
-        g.user = session['user']
+        g.member = session['user']
 
 
 @app.route('/logout')
@@ -107,14 +106,15 @@ def logout():
     session.pop('user', None)
     return render_template("login.html")
 
+
 @app.route('/cat_register', methods=['GET', 'POST'])
 def category_register():
     """ Method to create a category """
-    if g.user:
+    if g.member:
         if request.method == "POST":
             cat_name = request.form['category_name']
-            owner = request.form['category_owner']
-            category_create = new_cat.category_register(cat_name,owner)
+            owner = g.member
+            category_create = new_cat.category_register(cat_name, owner)
 
             category_data = new_cat.recipe_categories
             render_category = []
@@ -129,15 +129,30 @@ def category_register():
                 return render_template("recipe-categories.html", success=message, data=category_data)
             elif category_create == "204,Category exists":
                 message = "Category exists"
-                return render_template("recipe-categories.html", msg=message)
+                return render_template("recipe-categories.html", msg=message, data=category_data)
             elif category_create == "205,Invalid Name":
                 message = "Invalid Category Name"
-                return render_template("recipe-categories.html", msg=message)
+                return render_template("recipe-categories.html", msg=message, data=category_data)
             else:
                 message = "unable to create category"
-                return render_template("recipe-categories", msg=message)
+                return render_template("recipe-categories.html", msg=message, data=category_data)
+        else:
+            category_data = new_cat.recipe_categories
+            render_category = []
+            for category in category_data:
+                if category_data[category]['owner'] == g.member:
+                    render_category.append(category)
+                    if render_category == []:
+                        url_for('/recipe-categories')
+
+            return render_template("recipe-categories.html", data=category_data)
 
     return render_template("login.html")
 
 
-
+@app.route('/view_category', methods=["POST", "GET"])
+def view_category():
+    if g.member:
+        category_name = request.form['category_name']
+        return render_template("recipes.html", message=category_name)
+    return render_template("login.html")
