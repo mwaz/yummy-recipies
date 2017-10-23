6e921f611715cdf70b8f2e0006a96fec49a37075
mwaz <amby.mwaz@gmail.com>
2017-10-23 05:12:22 +0300

    [TEST] add tests to check the register category names while validating input

--- test_recipes.py
+++ test_recipes.py
@@ -36,3 +36,39 @@ class RecipeTest(unittest.TestCase):
         """Test to check if category_name follows the one specified by regex"""
         category_registration = self.newCat.category_register("&#*", "waweru@gmail.com")
         self.assertIn("205,Regex mismatch", category_registration, "205,Regex mismatch")
+
+
+    def test_recipe_registration(self):
+        """ Test for method create recipe """
+        self.newCat.recipe = {}
+        self.newCat.recipe_register("category_one", "recipe1", "waweru@gmail.com")
+        recipe_registration = self.newCat.recipe_register("category", "recipe2" , "waweru@gmail.com")
+        self.assertIn("200,OK", recipe_registration, "200,OK")
+
+    def test_recipe_regex_match(self):
+        """ Test for recipe name regex match """
+        recipe_registration = self.newCat.recipe_register("categor@y", "recipe1", "waweru@gmail.com")
+        self.assertIn("205,Regex mismatch", recipe_registration, "205,Regex mismatch")
+
+    def test_recipe_empty_name(self):
+        """ Test for empty recipe name  """
+        recipe_null = self.newCat.recipe_register(" ", "recipe1", "waweru@gmail.com")
+        self.assertIn("205,Invalid Name", recipe_null, "205,Invalid Name")
+
+    def test_recipe_null_name(self):
+        """ Test for null recipe name  """
+        recipe_null = self.newCat.recipe_register(' ', "recipe", "waweru@gmail.com")
+        self.assertIn("205,Invalid Name", recipe_null, "205,Invalid Name")
+
+    def test_recipe_exists(self):
+        """ Test for method if recipe exists """
+        self.newCat.recipe = {}
+        self.newCat.recipe_register("category", "recipe1", "waweru@gmail.com")
+        recipe_registration = self.newCat.recipe_register("category", "recipe1", "waweru@gmail.com")
+        self.assertIn("204,Recipe exists", recipe_registration, "204,Recipe exists")
+
+
+
+
+
+
