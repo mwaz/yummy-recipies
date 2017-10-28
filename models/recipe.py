25f8fc16197d86ba2dc757baf50d011443995838
mwaz <amby.mwaz@gmail.com>
2017-10-28 22:52:21 +0300

    [FIX] remodell the methods of category update, recipe update and and category delete to use lists

--- models/recipe.py
+++ models/recipe.py
@@ -7,7 +7,7 @@ class Recipe(object):
       Users Class to handle methods to do with recipes categories and actual recipes
       """
     recipe_categories = []
-    recipes = {}
+    recipes = []
 
     def __init__(self, cat_name=None, owner=None, recipe_name=None):
         """ Initializing  class instance variables"""
@@ -30,10 +30,10 @@ class Recipe(object):
             return "205,Invalid Name"
         return "205,Regex mismatch"
 
-    def category_delete(self, cat_name):
+    def category_delete(self, cat_name, owner):
         """ method that defines the elements required delete a category """
         for category_list in self.recipe_categories:
-            if category_list[0] == cat_name:
+            if category_list[0] == cat_name and category_list[1] == owner:
                 category_index = self.recipe_categories.index(category_list)
                 self.recipe_categories.pop(category_index)
                 return "200,OK"
@@ -43,22 +43,23 @@ class Recipe(object):
         """ method that defines the elements required to create an account """
         if re.match(regex_name, cat_name):
             if recipe_name != '' and recipe_name.strip():
-                if recipe_name not in self.recipes:
-                    self.recipes[recipe_name] = \
-                        {"cat_name": cat_name,
-                         "recipe_name": recipe_name,
-                         "owner": owner}
-                    return "200,OK"
-                return "204,Recipe exists"
+                if self.recipes != []:
+                    if (any(recipe_name == recipe_list[1] and owner == recipe_list[2] and cat_name == recipe_list[0] for recipe_list in self.recipes)) != True:
+                        self.recipes.append([cat_name, recipe_name, owner])
+                        return "200,OK"
+                    return "204,Recipe exists"
+                self.recipes.append([cat_name, recipe_name, owner])
             return "205,Invalid Name"
         return "205,Regex mismatch"
 
-    def recipe_delete(self, recipe_name):
-        """ method that defines the elements required to create an account """
-        if recipe_name in self.recipes.keys():
-            del self.recipes[recipe_name]
-            return "200,OK"
-        return "404,Recipe doesnt exist"
+    def recipe_delete(self, recipe_name, cat_name, owner):
+        """ method that defines the elements required to delete a recipe """
+        for recipe_list in self.recipes:
+            if recipe_list[1] == recipe_name and recipe_list[0] == cat_name and recipe_list[2] == owner :
+                recipe_index = self.recipes.index(recipe_list)
+                print(recipe_index)
+                self.recipes.pop(recipe_index)
+                return "200,OK"
 
     def view_recipe_category(self, owner):
         category_data = self.recipe_categories
@@ -68,19 +69,28 @@ class Recipe(object):
                 render_category.append(category)
         return render_category
 
-    def view_recipe(self, cat_name):
+    def view_recipe(self, cat_name, owner):
         recipe_data = self.recipes
         render_recipe = []
         for recipe in recipe_data:
-            if recipe_data[recipe]['cat_name'] == cat_name:
+            if recipe[0] == cat_name and recipe[2] == owner :
                 render_recipe.append(recipe)
         return render_recipe
 
-    def category_edit(self, cat_name):
+    def category_edit(self, cat_name, new_cat_name, owner):
         """ method that defines the elements required delete a category """
-        for category_list in self.recipe_categories:
-            if category_list[0] == cat_name:
-                category_index = self.recipe_categories.index(category_list)
-                self.recipe_categories.pop(category_index)
-                return "200,OK"
-                # return "404,Category doesnt exist"
+        if new_cat_name != '' and new_cat_name.strip():
+            if re.match(regex_name, new_cat_name):
+                for category_list in self.recipe_categories:
+                    if category_list[0] == cat_name and category_list[1] == owner:
+                        if (any(new_cat_name == category_list[0]
+                                for category_list in self.recipe_categories)) != True:
+                            category_list[0] = new_cat_name
+                            print(new_cat_name)
+                            return "200,OK"
+                        return "204,Category exists"
+                    return "205,Invalid Name"
+            return "205,Regex mismatch"
+        return "205,Invalid Name"
+
+
