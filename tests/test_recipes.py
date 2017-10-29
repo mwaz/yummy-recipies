c0e1a270020e30608954f118580dfb59864f067a
mwaz <amby.mwaz@gmail.com>
2017-10-29 23:08:45 +0300

    [TEST] add tests to check the editing of recipes and category names

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -41,7 +41,7 @@ class RecipeTest(unittest.TestCase):
 
     def test_recipe_registration(self):
         """ Test for method create recipe """
-        self.newCat.recipe = {}
+        self.newCat.recipe = []
         self.newCat.recipe_register("category_one", "recipe1", "waweru@gmail.com")
         recipe_registration = self.newCat.recipe_register("category", "recipe2" , "waweru@gmail.com")
         self.assertIn("200,OK", recipe_registration, "200,OK")
@@ -63,10 +63,40 @@ class RecipeTest(unittest.TestCase):
 
     def test_recipe_exists(self):
         """ Test for method if recipe exists """
-        self.newCat.recipe = {}
+        self.newCat.recipe = []
         self.newCat.recipe_register("category", "recipe1", "waweru@gmail.com")
         recipe_registration = self.newCat.recipe_register("category", "recipe1", "waweru@gmail.com")
         self.assertIn("204,Recipe exists", recipe_registration, "204,Recipe exists")
+
+    def test_update_category_empty_name(self):
+        """ Test for empty category name on update  """
+        category_null = self.newCat.category_edit("category_one", " ", "mwaz")
+        self.assertIn("205,Invalid Name", category_null, "205,Invalid Name")
+
+    def test_update_recipe_empty_name(self):
+        """ Test for empty recipe name on update  """
+        recipe_null = self.newCat.recipe_edit(" ", "category_one",  "mwaz")
+        self.assertIn("205,Invalid Name", recipe_null, "205,Invalid Name")
+
+    def test_update_recipe_regex(self):
+        """ Test for recipe name regex pattern name on update  """
+        recipe_regex = self.newCat.recipe_edit("@@@", "category_one", "mwaz")
+        self.assertIn("205,Regex mismatch", recipe_regex, "205,Regex mismatch")
+
+    def test_update_category_regex(self):
+        """ Test for category name regex pattern name on update  """
+        category_regex = self.newCat.recipe_edit("category_one", "@@@", "mwaz")
+        self.assertIn("205,Regex mismatch", category_regex, "205,Regex mismatch")
+
+    def test_update_category_exists(self):
+        """ Test for invalid  category name on update  """
+        recipe_exists = self.newCat.category_edit("categoryone", "", "mwaz")
+        self.assertEqual("205,Invalid Name", recipe_exists,"205,Invalid Name")
+
+    def test_update_recipe_exists(self):
+        """ Test for invalid  recipe name on update  """
+        recipe_exists = self.newCat.recipe_edit("","categoryone", "mwaz")
+        self.assertEqual("205,Invalid Name", recipe_exists,"205,Invalid Name")
 
 
 
