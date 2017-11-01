e8534458dd0f63ffadd2fd26c5545edc6fcf2454
mwaz <amby.mwaz@gmail.com>
2017-11-01 09:16:23 +0300

    [FIX] restructure recipes.py
    The commit will remove the iterative if statements in a closed loop and
    use a complete if statement if a condition fails. The methods changed are
    the recipe register, category register, recipe edit and the category edit

--- models/recipe.py
+++ models/recipe.py
@@ -17,38 +17,47 @@ class Recipe(object):
 
     def category_register(self, cat_name, owner):
         """ method that defines the elements required to create an account """
-        if re.match(regex_name, cat_name):
-            if cat_name != '' and cat_name.strip():
-                if self.recipe_categories != []:
-                    if (any(cat_name == category_list[0] and owner == category_list[1]
-                            for category_list in self.recipe_categories)) != True:
-                        self.recipe_categories.append([cat_name.replace(" ", "_"), owner,])
-                        return "200,OK"
-                    return "204,Category exists"
-                self.recipe_categories.append([cat_name.replace(" ", "_"), owner, ])
-                return "200,OK"
+        if not(cat_name):
             return "205,Invalid Name"
-        return "205,Regex mismatch"
+
+        if not re.search(regex_name, cat_name):
+            return "205,Regex mismatch"
+
+        if self.recipe_categories == []:
+            self.recipe_categories.append([cat_name.replace(" ", "_"), owner, ])
+            return "200,OK"
+            
+        if (any(cat_name == category_list[0] and owner == category_list[1]
+            for category_list in self.recipe_categories)) == True:
+            return "204,Category exists"
+
+        self.recipe_categories.append([cat_name.replace(" ", "_"), owner,])
+        return "200,OK"
+
 
     def category_edit(self, cat_name, new_cat_name, owner):
         """ method that defines the elements required delete a category """
-        if new_cat_name != '' and new_cat_name.strip():
-            if re.match(regex_name, new_cat_name):
-                for category_list in self.recipe_categories:
-                    if category_list[0] == cat_name and category_list[1] == owner:
-                        if (any(new_cat_name == category_list[0]
-                                for category_list in self.recipe_categories)) != True:
-                            category_list[0] = new_cat_name.replace(" ", "_")
+        if not(cat_name):
+            return "205,Invalid Name"
 
-                            for recipe_list in self.recipes:
-                                recipe_index = self.recipes.index(recipe_list)
-                                self.recipes[recipe_index][0] = new_cat_name.replace(" ", "_")
-
-                            return "200,OK"
-                        return "204,Category exists"
-                    return "205,Invalid Name"
+        if not re.search(regex_name, new_cat_name):
             return "205,Regex mismatch"
-        return "205,Invalid Name"
+
+        for category_list in self.recipe_categories:
+            if category_list[0] == cat_name and category_list[1] == owner:
+                category_list[0] = new_cat_name.replace(" ", "_")
+
+                for recipe_list in self.recipes:
+                    recipe_index = self.recipes.index(recipe_list)
+                    self.recipes[recipe_index][0] = new_cat_name.replace(" ", "_")
+                return "200,OK"
+                return "200,OK,UPDATE"
+            
+        # for recipe_list in self.recipes:
+        #     recipe_index = self.recipes.index(recipe_list)
+        #     self.recipes[recipe_index][0] = new_cat_name.replace(" ", "_")
+        #     return "200,OK"
+
 
     def category_delete(self, cat_name, owner):
         """ method that defines the elements required delete a category """
@@ -57,37 +66,49 @@ class Recipe(object):
                 category_index = self.recipe_categories.index(category_list)
                 self.recipe_categories.pop(category_index)
                 return "200,OK"
-            # return "404,Category doesnt exist"
+          
 
     def recipe_register(self, cat_name, recipe_name, owner):
         """ method that defines the elements required to create an account """
-        if re.match(regex_name, recipe_name):
-            if recipe_name != '':
-                if self.recipes != []:
-                    if (any(recipe_name == recipe_list[1] and owner == recipe_list[2]
-                            and cat_name == recipe_list[0]
-                            for recipe_list in self.recipes)) != True:
-                        self.recipes.append([cat_name, recipe_name.replace(" ", "_"), owner, ])
-                        return "200,OK"
-                    return "204,Recipe exists"
-                self.recipes.append([cat_name, recipe_name.replace(" ", "_"), owner])
-                return "200,OK"
+        if not (recipe_name):
             return "205,Invalid Name"
-        return "205,Regex mismatch"
 
-    def recipe_edit(self, new_recipe_name, cat_name, owner):
-        """ method that defines the elements required to edit a recipe"""
-        if new_recipe_name != '' and new_recipe_name.strip():
-            if re.match(regex_name, new_recipe_name):
-                for recipe_list in self.recipes:
-                    recipe_index = self.recipes.index(recipe_list)
-                    if (any(new_recipe_name == recipe_list[1]
-                            for recipe_list in self.recipes)) != True:
-                        self.recipes[recipe_index][1] = new_recipe_name.replace(" ", "_")
-                        return "200,OK"
-                    return "204,Recipe exists"
+        if not re.search(regex_name, recipe_name):
             return "205,Regex mismatch"
-        return "205,Invalid Name"
+
+        if self.recipes == []:
+            self.recipes.append([cat_name, recipe_name.replace(" ", "_"), owner])
+            return "200,OK"
+
+        if (any(recipe_name == recipe_list[1] 
+            and owner == recipe_list[2] 
+            and cat_name == recipe_list[0] 
+            for recipe_list in self.recipes)) == True:
+            return "204,Recipe exists"
+
+        self.recipes.append([cat_name, recipe_name.replace(" ", "_"), owner, ])
+        return "200,OK"
+
+    def recipe_edit(self, new_recipe_name, recipe_name, cat_name, owner):
+        """ method that defines the elements required to edit a recipe"""
+        if not (new_recipe_name):
+            return "205,Invalid Name"
+
+        if not re.search(regex_name, new_recipe_name):
+            return "205,Regex mismatch"
+
+        for recipe_list in self.recipes:
+           
+            # if (any(new_recipe_name == recipe_list[1] for
+            #  recipe_list in self.recipes)) == True:
+            #     return "204,Recipe exists"
+            
+            print (recipe_list[1] + "THIS IS RECIPE LIST")
+            print (recipe_name + "THIS IS RECIPE NAME")
+            if recipe_list[1]==recipe_name:
+                recipe_list[1] = new_recipe_name.replace(" ", "_")
+                return "200,OK"
+
 
     def recipe_delete(self, recipe_name, cat_name, owner):
         """ method that defines the elements required to delete a recipe """
@@ -99,16 +120,16 @@ class Recipe(object):
 
     def view_recipe_category(self, owner):
         category_data = self.recipe_categories
-        render_category = []
+        render_category_data = []
         for category in category_data:
             if category[1] == owner:
-                render_category.append(category)
-        return render_category
+                render_category_data.append(category)
+        return render_category_data
 
     def view_recipe(self, cat_name, owner):
         recipe_data = self.recipes
-        render_recipe = []
+        render_recipe_data = []
         for recipe in recipe_data:
             if recipe[0] == cat_name and recipe[2] == owner :
-                render_recipe.append(recipe)
-        return render_recipe
+                render_recipe_data.append(recipe)
+        return render_recipe_data
