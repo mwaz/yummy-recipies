810644ef282a9a8fc1a3c8ccec26aca041167994
mwaz <amby.mwaz@gmail.com>
2017-11-01 09:08:59 +0300

    [FIX] change the structure of the iterating If stattements and the methods to fetch username and email to ternary statements

--- models/user.py
+++ models/user.py
@@ -1,65 +1,70 @@
 import re
 
 users = {}
-regex_member = "[a-zA-Z0-9- .]+$"
+users['mwaz@gmail.com'] = {'username': 'mwaz', 'email': 'mwaz@gmail.com', 'password': '1234567q'}
+regex_username = "[a-zA-Z0-9- .]+$"
 regEmail = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-
-
 
 class Users(object):
     """
-      Users Class to handle  user functions like login and signup
-      """
-    def __init__(self, member=None, email=None, password=None):
+    Users Class to handle  user functions like login and signup
+    """
+    
+    def __init__(self, username=None, email=None, password=None):
         """ Initializing  class instance variables"""
-        self.member = member
+        self.username= username
         self.email = email
         self.password = password
 
-    def member_register(self, email, member, password, cpassasword):
+    def user_register(self, email, username, password, cpassasword):
         """ method that defines the elements required
          to create a member account
-          """
-        if re.match(regex_member, member):
-            if member != '' and email != '' and password != '' and cpassasword != '' and cpassasword != ' ':
-                if member not in users.keys():
-                    if email not in users.keys():
-                        if password == cpassasword:
-                            if re.search(regEmail, email):
-                                if len(password) >= 8:
-                                    users[email] = {'member': member, 'email': email, 'password': password}
-                                    return "200,OK"
-                                return "205,Password Regex mismatch"
-                            return "205,Email Regex mismatch"
-                        return "400,Passwords dont match"
-                    return "401,Email exists"
-                return "401,Member exists"
+        """
+        if not  (username and email and password and cpassasword):
             return "205,Invalid input"
-        return "205,Regex mismatch"
 
+        if not re.search(regex_username, username):
+            return "205,Regex mismatch"
 
-    def get_member(self, email):
+        if username in users.keys():
+            return "401,username exists"
+        
+        if not re.search(regEmail, email):
+            return "205,Email Regex mismatch"
+
         if email in users.keys():
-            result_user = users[email]
-            return result_user['member']
-        return False
+            return "401,Email exists"    
 
-    def get_email(self, email):
-        if email in users.keys():
-            result_email = users[email]
-            return result_email['email']
-        return False
+        if password != cpassasword:
+            return "400,Passwords dont match"
+        else:
+            if len(password) < 8:
+                return "205,Password Regex mismatch"
+
+        
+        users[email] = {'username': username, 'email': email, 'password': password}
+        print (users)
+        return "200,OK"
 
     def user_login(self, email, password):
         """Method to login member"""
-        if email != ' ' and password != ' ':
-            if email != '' and password != '':
-                if email in users.keys():
-                    result = users[email]
-                    passwd = result['password']
-                    if passwd == password:
-                        return "200,OK"
-                    return "205,Password mismatch"
-                return "404,User not found"
-            return "205,Invalid input"
-        return "205,Empty input"
+
+        if not (email and password):
+            return "205,Empty input"
+        if email not in users.keys():
+            return "404,User not found"
+        
+        result = users[email]
+        passwd = result['password']
+        if passwd != password:
+            return "205,Password mismatch"
+        else:
+            return "200,OK"
+
+    def  get_registered_user_email(self, email):
+        user_details = users[email] if email in users.keys() else False
+        return user_details['email']
+
+    def  get_registered_user_name(self, email):
+        user_details = users[email] if email in users.keys() else False
+        return user_details['username']
