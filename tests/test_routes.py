87e5ff1f43d5eb1eec922d82914a005b40509fad
mwaz <amby.mwaz@gmail.com>
2017-11-03 02:54:52 +0300

    [TEST] add test to check if the default location exists

---
+++ tests/test_routes.py
@@ -0,0 +1,19 @@
+import unittest
+
+from app import app
+
+class TestUsers(unittest.TestCase):
+    """
+    class to test the class user
+    """
+
+    def setUp(self):
+        """ Method called to prepare the test fixture """
+   
+        app.config['TESTING'] = True
+        self.test_app = app.test_client()
+
+    def test_root_route(self):
+        """ test to see that homepage loads"""
+        response = self.test_app.get('/')
+        self.assertIn(b'Index', response.data)
