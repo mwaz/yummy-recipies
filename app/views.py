e0f53bb18604e464cef70a2cfc6ac3e08cac888d
mwaz <amby.mwaz@gmail.com>
2017-11-03 02:56:03 +0300

    [FIX] remove status codes and replace them with strings

--- app/views.py
+++ app/views.py
@@ -2,13 +2,15 @@ import os
 from flask import render_template, request, session, g, Flask
 from models.user import Users
 from models.recipe import Recipe
+from models.categories import Categories
 app = Flask(__name__)
 from app import app
 
 app.secret_key = os.urandom(24) #needed to keep the client sessions secure
 
 new_user = Users() #instance of class Users
-new_cat = Recipe() #instance of class Recipe
+new_cat = Categories() #instance of class Category
+new_recipe = Recipe() #instance of class Recipe
 
 
 @app.route('/')
@@ -37,41 +39,36 @@ def register():
         #then returned to the user in the registration or login templates
         #depending on the result
         result = new_user.user_register(email, username, password, cpassword)
-        if result == "200,OK":
+        if result == "Successfully created account":
             session['user'] = username
             msg_output = "Successfully created Account"
             return render_template('login.html', success=msg_output)
 
-        elif result == "205,Invalid input":
+        elif result == "please input all fields":
             msg_output = "please fill in all the fields"
             return render_template("registration.html", msg=msg_output)
 
-        elif result == "205,Empty input":
-            msg_output = "please fill in all the fields"
-            return render_template("registration.html", msg=msg_output)
-
-
-        elif result == "205,Regex mismatch":
+        elif result == "username should not have special characters or spaces":
             msg_output = "Special characters are not allowed in username field"
             return render_template("registration.html", msg=msg_output)
 
-        elif result == "205,Password Regex mismatch":
+        elif result == "password length should be atleast 8 characters":
             msg_output = "Password should have atleat 8 characters"
             return render_template("registration.html", msg=msg_output)
 
-        elif result == "401,username exists":
-            msg_output = "member name already taken"
+        elif result == "username exists":
+            msg_output = "user name already taken"
             return render_template("registration.html", msg=msg_output)
 
-        elif result == "400,Passwords dont match":
+        elif result == "Passwords dont match":
             msg_output = "Passwords dont match"
             return render_template("registration.html", msg=msg_output)
 
-        elif result == "205,Email Regex mismatch":
+        elif result == "Email format is invalid":
             msg_output = "Email is not a valid email in the format someone@someone.com"
             return render_template("registration.html", msg=msg_output)
 
-        elif result == "401,Email exists":
+        elif result == "Email exists":
             msg_output = "Email already exists"
             return render_template("registration.html", msg=msg_output)
 
@@ -90,11 +87,12 @@ def login():
         password = request.form['password']
         result_login = new_user.user_login(email, password)
 
-        if result_login == "200,OK":
-            username = new_user.get_registered_user_name(email)
-            email = new_user.get_registered_user_email(email)
-            session['user'] = username
-            session['email'] = email
+        if result_login == "Success login":
+            username = new_user.get_registered_user_details(email)
+            email = new_user.get_registered_user_details(email)
+            session['user'] = username['username']
+
+            session['email'] = email['email']
             message = "login successful"
 
             #method in the Users class to display all the recipe categories of a user
@@ -105,15 +103,15 @@ def login():
             else:
                 return render_template("recipe-categories.html")
 
-        elif result_login == "205,Password match":
+        elif result_login == "Password mismatch":
             message = "Invalid login credentials"
             return render_template("login.html", msg=message)
 
-        elif result_login == "404,User not found":
+        elif result_login == "email not found":
             message = "User does not exist, kindly try again"
             return render_template("login.html", msg=message)
 
-        elif result_login == "205,Empty Input":
+        elif result_login == "empty email or password fields":
             message = "Kindly fill all fields"
             return render_template("login.html", msg=message)
         else:
@@ -142,20 +140,23 @@ def category_register():
             category_create = new_cat.category_register(cat_name, owner)
             category_data = new_cat.view_recipe_category(g.owner)
 
-            if category_create == "200,OK":
+            if category_create == "Successfully created category":
                 message = "Successfully created category"
                 return render_template("recipe-categories.html", success=message, data=category_data)
             
-            elif category_create == "200,OK,UPDATE":
-                message = "Successfully updated category"
-                return render_template("recipe-categories.html", success=message, data=category_data)
-
-            elif category_create == "204,Category exists":
+            elif category_create == "Category exists":
                 message = "Category exists"
                 return render_template("recipe-categories.html", msg=message, data=category_data)
-            elif category_create == "205,Invalid Name":
-                message = "Invalid Category Name"
+            
+            elif category_create == "Category name is null":
+                message = "Kndly enter the category name"
                 return render_template("recipe-categories.html", msg=message, data=category_data)
+            
+            elif category_create == "category name has special characters":
+                message = "Category name should only have letters"
+                return render_template("recipe-categories.html", msg=message, data=category_data)
+            
+
             else:
                 message = "unable to create category"
                 return render_template("recipe-categories.html", msg=message, data=category_data)
@@ -174,40 +175,10 @@ def view_category(category_name):
     if g.owner:
         #The category name is obtained from the recipe_categories.html templates using GET
         category_name = category_name
-        view_cat = new_cat.view_recipe(category_name, g.owner)
+        view_cat = new_recipe.view_recipe(category_name, g.owner)
         return render_template("recipes.html", message=category_name, data=view_cat)
     return render_template("login.html")
 
-
-@app.route('/recipe_register', methods=['GET', 'POST'])
-def recipe_register():
-    """method to create a recipe """
-    #executes if the logged in user has a current session
-    if g.owner:
-        if request.method == "POST":
-            recipe_name = request.form['recipe_name']
-            cat_name = request.form['category_name']
-            owner = g.owner
-            recipe_create = new_cat.recipe_register(cat_name, recipe_name, owner)
-
-            render_recipe = new_cat.view_recipe(cat_name, g.owner)
-            if recipe_create == "200,OK":
-                message = "Successfully created recipe"
-                return render_template("recipes.html", success=message, data=render_category, message=cat_name)
-            elif recipe_create == "204,Recipe exists":
-                message = "Recipe exists"
-                return render_template("recipes.html", msg=message, data=render_category, message=cat_name)
-            elif recipe_create == "205,Invalid Name":
-                message = "Invalid Recipe Name"
-                return render_template("recipes.html", msg=message, data=render_category, message=cat_name)
-            elif recipe_create == "205,Regex mismatch":
-                message = "Recipe Name has special characters "
-                return render_template("recipes.html", msg=message, data=render_category, message=cat_name)
-            else:
-                message = "unable to create recipe"
-                return render_template("recipes.html", msg=message, data=render_category, message=cat_name)
-        return render_template("recipes.html")
-    return render_template("login.html")
 
 
 @app.route('/category_edit/<category_name>', methods=['GET', 'POST'])
@@ -217,34 +188,29 @@ def category_edit(category_name):
         if request.method == "POST":
             cat_name = category_name
             new_cat_name = request.form['cat_name']
-            recipe_edit = new_cat.category_edit(cat_name, new_cat_name, g.owner)
+            category_result = new_cat.category_edit(cat_name, new_cat_name, g.owner)
             data = new_cat.view_recipe_category(g.owner)
-
             
-            if recipe_edit == "200,OK":
+            if category_result == "successfully updated category name":
                 message = "Successfully edited category"
                 return render_template("recipe-categories.html", success=message, data=data)
 
-            if recipe_edit == "200,OK,UPDATE":
-                message = "Successfully edited category"
-                return render_template("recipe-categories.html", success=message, data=data)
-
-
-            elif recipe_edit == "204,Category exists":
+            elif category_result == "Category exists":
                 message = "Can't edit category! Category Name Exists"
                 return render_template("recipe-categories.html", msg=message, data=data)
 
-            elif recipe_edit == "205,Invalid Name":
-                message = "Invalid category name"
+            elif category_result == "Null category name":
+                message = "Category name is NOT provided"
                 return render_template("recipe-categories.html", msg=message, data=data)
 
-            elif recipe_edit == "205,Regex mismatch":
-                message = "Regex mismatch"
+            elif category_result == "category name exists":
+                message = "Category Name exists"
                 return render_template("recipe-categories.html", msg=message, data=data)
 
-            elif recipe_edit == "404,Cant Edit Category":
-                message = "Cannot Edit Category"
+            elif category_result == "category name has special characters":
+                message = "Category name should only have letters"
                 return render_template("recipe-categories.html", msg=message, data=data)
+
             else:
                 message = "unable to edit recipe category"
                 return render_template("recipe-categories.html", msg=message, data=data)
@@ -261,10 +227,10 @@ def category_delete():
             delete_result = new_cat.category_delete(category_name, g.owner)
             data = new_cat.view_recipe_category(g.owner)
 
-            if delete_result == "200,OK":
+            if delete_result == "successfully deleted category":
                 msg = "Recipe Category Deleted"
                 return render_template("recipe-categories.html", msg=msg, data=data)
-            elif delete_result == "404,Category doesnt exist":
+            elif delete_result == "Category doesnt exist":
                 msg = "Recipe category does not exist"
                 return render_template("recipe-categories.html", msg=msg, data=data)
             else:
@@ -274,6 +240,48 @@ def category_delete():
     return render_template("login.html")
 
 
+@app.route('/recipe_register', methods=['GET', 'POST'])
+def recipe_register():
+    """method to create a recipe """
+    #executes if the logged in user has a current session
+    if g.owner:
+        if request.method == "POST":
+            recipe_name = request.form['recipe_name']
+            cat_name = request.form['category_name']
+            recipe_ingredients = request.form['recipe_ingredients']
+            owner = g.owner
+            recipe_create = new_recipe.recipe_register(cat_name, recipe_name, owner, recipe_ingredients)
+
+            render_recipe = new_recipe.view_recipe(cat_name, g.owner)
+            if recipe_create == "successfully created recipe":
+                message = "Successfully created recipe"
+                return render_template("recipes.html", success=message, data=render_recipe, message=cat_name)
+            
+            elif recipe_create == "Recipe exists":
+                message = "Recipe exists"
+                return render_template("recipes.html", msg=message, data=render_recipe, message=cat_name)
+            
+            elif recipe_create == "Null recipe name":
+                message = "Recipe name is NOT provided"
+                return render_template("recipes.html", msg=message, data=render_recipe, message=cat_name)
+            
+            elif recipe_create == "Null recipe ingredients":
+                message = "Kindly provide recipe ingridients for the recipe"
+                return render_template("recipes.html", msg=message, data=render_recipe, message=cat_name)
+            
+
+            elif recipe_create == "Recipe name has special characters":
+                message = "Recipe Name should only have letters "
+                return render_template("recipes.html", msg=message, data=render_recipe, message=cat_name)
+            
+            else:
+                message = "unable to create recipe"
+                return render_template("recipes.html", msg=message, data=render_recipe, message=cat_name)
+        return render_template("recipes.html")
+    return render_template("login.html")
+
+
+
 @app.route('/recipe_delete', methods=['GET', 'POST'])
 def recipe_delete():
     """method to delete a recipe"""
@@ -281,17 +289,15 @@ def recipe_delete():
         if request.method == "POST":
             data = request.form['categor_name']
             recipe_name = request.form['recipe_name']
-            delete_result = new_cat.recipe_delete(recipe_name, data, g.owner)
-            recipe_data = new_cat.view_recipe(data, g.owner)
+            delete_result = new_recipe.recipe_delete(recipe_name, data, g.owner)
+            recipe_data = new_recipe.view_recipe(data, g.owner)
 
-            if delete_result == "200,OK":
+            if delete_result == "Successfully deleted recipe":
                 msg = "Recipe Deleted"
-                return render_template("recipes.html", msg=msg, message=data, data=recipe_data)
-            elif delete_result == "404,Recipe doesnt exist":
-                msg = "Recipe does not exist"
-                return render_template("recipes.html", msg=msg, message=data, data=recipe_data)
+                return render_template("recipes.html", msg=msg, message=data, data=recipe_data)      
+            
             else:
-                msg = "Can't delete recipe"
+                msg = "unable to delete recipe"
                 return render_template("recipes.html", msg=msg, message=data, data=recipe_data)
         return render_template("recipes.html")
     return render_template("login.html")
@@ -303,33 +309,32 @@ def recipe_edit(category_name):
     if g.owner:
         cat_name = category_name
         owner = g.owner
-        data = new_cat.view_recipe(cat_name, owner)
+        data = new_recipe.view_recipe(cat_name, owner)
         recipe_name = request.form['recipe_name']
+        recipe_ingredients = request.form['recipe_ingredients']
+
         if request.method == "POST":
             new_recipe_name = request.form['new_recipe_name']
-            edit_recipe = new_cat.recipe_edit(new_recipe_name, recipe_name, cat_name, owner)
+            edit_recipe = new_recipe.recipe_edit(new_recipe_name, recipe_name, cat_name, owner, recipe_ingredients)
 
-            if edit_recipe == "200,OK":
+            if edit_recipe == "Successfully edited recipe":
                 message = "Successfully edited recipe"
                 return render_template("recipes.html",  message=cat_name, success=message, data=data)
 
-            elif edit_recipe == "204,Recipe exists":
+            elif edit_recipe == "Recipe exists":
                 error = "Can't edit Recipe! Recipe Name Exists"
                 return render_template("recipes.html", message=cat_name, msg=error, data=data)
 
-            elif edit_recipe == "205,Invalid Name":
+            elif edit_recipe == "Null recipe name":
                 error = "Invalid Recipe name"
                 return render_template("recipes.html", message=cat_name, msg=error, data=data)
 
-            elif edit_recipe == "205,Regex mismatch":
-                error = "Regex mismatch"
+            elif edit_recipe == "Recipe name has special characters":
+                error = "Recipe name should only have letters"
                 return render_template("recipes.html", message=cat_name, msg=error, data=data)
 
-            elif edit_recipe == "401,Process error":
-                error = "Cannot Edit Recipe, Invalid token"
-                return render_template("recipes.html", message=cat_name, msg=error, data=data)
             else:
-                error = "unable to edit recipe "
+                error = "unable to edit recipe"
                 return render_template("recipes.html", message=cat_name, msg=error, data=data)
 
         return render_template("recipes.html", message=cat_name, data=data)
