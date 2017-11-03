f75736dfd82f0319be2ea15310a6c95140cd93ff
mwaz <amby.mwaz@gmail.com>
2017-11-03 03:01:06 +0300

    [FIX] remove status codes and replace them with strings

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -1,97 +1,61 @@
 import unittest
 from models.user import Users
 from models.recipe import Recipe
+from models.categories import Categories
 
 
-class RecipeTest(unittest.TestCase, Users):
+class RecipeTest(unittest.TestCase, Categories):
     """ Class performing unit testing for class Recipe"""
 
     def setUp(self):
         """Defining setUp() method that runs prior to each test."""
-        self.newCat = Recipe()
-        self.recipe_register = self.newCat.recipe_register(
-            "category", "recipe1", "waweru@gmail.com")
-
-    def test_category_registration(self):
-        """ Test to test function create category """
-        self.newCat.recipe_categories = []
-        self.newCat.category_register("category_one", "waweru@gmail.com")
-        category_registration = self.newCat.category_register(
-            "category", "waweru@gmail.com")
-        self.assertIn("200,OK", category_registration, "200,OK")
-
-    def test_category_exists(self):
-        """ Test to check if category_name exists """
-        self.newCat.recipe_categories = []
-        self.newCat.category_register("category", "waweru@gmail.com")
-        category_registration = self.newCat.category_register(
-            "category", "waweru@gmail.com")
-        self.assertIn("204,Category exists",
-                      category_registration, "204,Category exists")
-
-    def test_null_category(self):
-        """ Test to test creation of a null category name"""
-        category_registration = self.newCat.category_register(
-            "", "waweru@gmail.com")
-        self.assertIn("205,Invalid Name",
-                      category_registration, "205,Invalid Name")
-
-    def test_invalid_category_name(self):
-        """Test to check if category_name follows the one specified by regex"""
-        category_registration = self.newCat.category_register(
-            "&#*", "waweru@gmail.com")
-        self.assertIn("205,Regex mismatch",
-                      category_registration, "205,Regex mismatch")
+        self.newRecipe = Recipe()
+        self.newCategory = Categories()
+        self.recipe_register = self.newRecipe.recipe_register(
+            "category", "recipe", "waweru@gmail.com", "recipe_ingredients")
+        self.newCategory.category_register("category_one", "waweru@gmail.com")
 
     def test_recipe_registration(self):
         """ Test for method create recipe """
-        self.recipe_register
-        recipe_registration = self.newCat.recipe_register(
-            "category", "recipe2", "waweru@gmail.com")
-        self.assertIn("200,OK", recipe_registration, "200,OK")
+        recipe_success_registration = self.newRecipe.recipe_register(
+            "category_one", "recipee", "waweru@gmail.com", "recipe_ingredients")
+        self.assertEqual("successfully created recipe",
+                         recipe_success_registration)
 
     def test_recipe_regex_match(self):
         """ Test for recipe name regex match """
-        recipe_registration = self.newCat.recipe_register(
-            "category", "@@@", "waweru@gmail.com")
-        self.assertIn("205,Regex mismatch",
-                      recipe_registration, "205,Regex mismatch")
+        recipe_name_regex_format = self.newRecipe.recipe_register(
+            "category", "@@@", "waweru@gmail.com", "recipe_ingredients")
+        self.assertEqual("Recipe name has special characters",
+                         recipe_name_regex_format)
 
     def test_recipe_null_name(self):
         """ Test for null recipe name  """
-        recipe_null = self.newCat.recipe_register(
-            "category", "", "waweru@gmail.com")
-        self.assertIn("205,Invalid Name", recipe_null, "205,Invalid Name")
+        recipe_null_name = self.newRecipe.recipe_register(
+            "category", "", "waweru@gmail.com","recipe_ingredients")
+        self.assertEqual("Null recipe name", recipe_null_name)
 
     def test_recipe_exists(self):
         """ Test for method if recipe exists """
+        recipe_exists = self.newRecipe.recipe_register(
+            "category", "recipe", "waweru@gmail.com", "recipe_ingredients")
+        self.assertEqual("Recipe exists", recipe_exists)
 
-        self.newCat.recipe_register("category", "recipe1", "waweru@gmail.com")
-        recipe_registration = self.newCat.recipe_register(
-            "category", "recipe1", "waweru@gmail.com")
-        self.assertIn("204,Recipe exists",
-                      recipe_registration, "204,Recipe exists")
-
-    def test_update_recipe_regex(self):
+    def test_edit_recipe_regex_format(self):
         """ Test for recipe name regex pattern name on update  """
-        recipe_regex = self.newCat.recipe_edit(
-            "@@@", "recipe_name", "category_one", "mwaz")
-        self.assertIn("205,Regex mismatch", recipe_regex, "205,Regex mismatch")
+        edit_recipe_regex = self.newRecipe.recipe_edit(
+            "@@@", "recipe_name", "category_one", "mwaz", "recipe_ingredients")
+        self.assertEqual("Recipe name has special characters",
+                         edit_recipe_regex)
 
-    def test_update_category_regex(self):
-        """ Test for category name regex pattern name on update  """
-        category_regex = self.newCat.category_edit(
-            "category_one", "@@@", "mwaz")
-        self.assertIn("205,Regex mismatch",
-                      category_regex, "205,Regex mismatch")
+    def test_edit_recipe_is_null(self):
+        """ Test for null recipe name on update  """
+        recipe_edit_is_null = self.newRecipe.recipe_edit(
+            "", "categoryone", "recipe_name", "mwaz", "recipe_ingredients")
+        self.assertEqual("Null recipe name", recipe_edit_is_null)
 
-    def test_update_category_exists(self):
-        """ Test for invalid  category name on update  """
-        recipe_exists = self.newCat.category_edit("categoryone", "", "mwaz")
-        self.assertEqual("205,Invalid Name", recipe_exists, "205,Invalid Name")
-
-    def test_update_recipe_exists(self):
-        """ Test for invalid  recipe name on update  """
-        recipe_exists = self.newCat.recipe_edit(
-            "", "categoryone", "recipe_name", "mwaz")
-        self.assertEqual("205,Invalid Name", recipe_exists, "205,Invalid Name")
+    def succss_recipe_name_edit(self):
+        """ Test for successful recipe name on update  """
+        recipe_edit_success = self.newRecipe.recipe_edit(
+            "new_recipe", "categoryone", "recipe_name", "mwaz", "recipe_ingredients")
+        self.assertEqual("Successfully edited recipe", recipe_edit_success)
