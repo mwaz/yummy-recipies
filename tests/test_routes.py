7df2b312d5bd0c8ef5063cf6b16d94ccf88dd393
mwaz <amby.mwaz@gmail.com>
2017-11-09 00:03:50 +0300

    [TESTS] add tests to test both the recipe category CRUD routes and the recipes CRUD routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,3 +18,49 @@ class TestUsers(unittest.TestCase):
         """ test to see that homepage loads"""
         response = self.test_app.get('/')
         self.assertIn(b'Index', response.data)
+
+
+    def test_login_route(self):
+        """ Test to check if the login route loads"""
+        response = self.test_app.get('/login')
+        self.assertEquals(response.status_code, 200)
+
+    def test_register_route(self):
+        """ Test to check if the user register route loads"""
+        response = self.test_app.get('/register')
+        self.assertEquals(response.status_code, 200)
+
+    def test_category_register_route(self):
+        """ Test to check if category register route works"""
+        response = self.test_app.get('/cat_register')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_category_route(self):
+        """ Test to check if view category route works"""
+        response = self.test_app.get('/view_category/Lunch')
+        self.assertEquals(response.status_code, 200)
+
+    def test_edit_category_route(self):
+        """ Test to check if view category route works"""
+        response = self.test_app.get('/category_edit/Lunch')
+        self.assertEquals(response.status_code, 200)
+
+    def test_delete_category_route(self):
+        """ Test to check if view category route works"""
+        response = self.test_app.get('/category_delete')
+        self.assertEquals(response.status_code, 200)
+
+    def test_recipe_register_route(self):
+        """ Test to check if recipe register route works"""
+        response = self.test_app.get('/recipe_register')
+        self.assertEquals(response.status_code, 200)
+
+    def test_recipe_edit_route(self):
+        """ Test to check if recipe edit route works"""
+        response = self.test_app.get('/recipe_edit/Panckakes')
+        self.assertEquals(response.status_code, 200)
+
+    def test_recipe_delete_route(self):
+        """ Test to check if recipe delete route works"""
+        response = self.test_app.get('/recipe_delete')
+        self.assertEquals(response.status_code, 200)
